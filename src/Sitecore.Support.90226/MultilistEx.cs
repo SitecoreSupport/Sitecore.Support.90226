89f5dccdfa70fd30896d39151de59d16a45113b6
sc-AmitPatel <ampa@sitecore.net>
2018-12-04 13:37:40 -0500

    Initial commit.

---
+++ src/Sitecore.Support.90226/MultilistEx.cs
@@ -0,0 +1,44 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentEditor
+{
+  using Sitecore;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Globalization;
+  using Sitecore.Resources;
+  using Sitecore.Shell.Applications.ContentEditor;
+  using Sitecore.Text;
+  using System;
+  using System.Collections;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web.UI;
+  public class MultilistEx : Sitecore.Shell.Applications.ContentEditor.MultilistEx
+  {
+    protected override void GetSelectedItems(Item[] sources, out System.Collections.ArrayList selected, out System.Collections.IDictionary unselected)
+    {
+      Assert.ArgumentNotNull(sources, "sources");
+      ListString str = new ListString(base.Value);
+      unselected = new System.Collections.SortedList();
+      List<Item> unselectedList = new List<Item>();
+      selected = new System.Collections.ArrayList(str.Count);
+      for (int i = 0; i < str.Count; i = (int)(i + 1))
+      {
+        selected.Add(str[i]);
+      }
+      foreach (Item item in sources)
+      {
+        string str2 = item.ID.ToString();
+        int index = str.IndexOf(str2);
+        if (index >= 0)
+        {
+          selected[index] = item;
+        }
+        else
+        {
+          unselectedList.Add(item);
+        }
+      }
+      unselected = unselectedList.ToDictionary(x => x);
+    }
+  }
+}
---
+++ src/Sitecore.Support.90226/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.90226")]
+[assembly: AssemblyProduct("Sitecore.Support.90226")]
+[assembly: ComVisible(false)]
